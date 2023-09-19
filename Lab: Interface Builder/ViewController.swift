70ccf5716716eb1a72b4a60ca89d0ab6fd26af8a
Jose Trujillo <joseh.trujillo11@gmail.com>
2023-09-19 14:00:17 -0400

    Initial Commit

---
+++ Lab: Interface Builder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab: Interface Builder
+//
+//  Created by 4d on 9/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
